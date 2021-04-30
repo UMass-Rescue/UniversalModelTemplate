f4f81c6d31f849250a20dc094f8df68d543b0856
Cody Richter <Vortekdynamic@gmail.com>
2021-04-30 17:24:05 -0400

    Update config.py

--- model/config.py
+++ model/config.py
@@ -5,7 +5,7 @@ model_name = 'example_model'
 # Tags are used to describe the performance of a model. These simple keywords can help people decide whether your model
 # is appropriate to use for their situation. Some examples of tags are 'fast', 'accurate', or 'essential'. You should
 # limit the number of tags your model has to only contain a few with relevant information.
-model_tags = ['tagA', 'tagB', 'tagC']
+model_tags = 'tagA,tagB,tagC'
 
 
 # The model type determines what inputs your model will receive. The options are:
